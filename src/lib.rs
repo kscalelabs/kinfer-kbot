a2d762d0ab7749678b617322c38b28ee330495e9
Aaron <aaron@kscale.dev>
2025-05-11 19:51:05 -0700

    back to info

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub mod runtime;
 
 pub fn initialize_logging() {
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("Setting default subscriber failed");
 }
